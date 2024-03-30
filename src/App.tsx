00a002e9e42391f152e0f95f93b26720e52989b1
Felix Bello <bello2redfield@gmail.com>
2024-03-30 10:49:06 +0700

    implement sort movie selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GamesGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,10 +6,12 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatforms";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
+    sortOrder: string;
 }
 
 function App() {
@@ -41,10 +43,16 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area="main" padding="12px">
-                <PlatformSelector
-                    selectedPlatform={gameQuery.platform}
-                    onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
-                />
+                <HStack spacing={3}>
+                    <PlatformSelector
+                        selectedPlatform={gameQuery.platform}
+                        onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
+                    />
+                    <SortSelector
+                        sortOrder={gameQuery.sortOrder}
+                        onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })}
+                    />
+                </HStack>
                 <GamesGrid gameQuery={gameQuery} />
             </GridItem>
         </Grid>
